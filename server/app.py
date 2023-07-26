2a83efeb5259390b98ac6a2a960715548a0d3558
Mohammad Arshad <mdarshad1000@gmail.com>
2023-07-26 21:28:53 +0530

    Include pinecone namespace for filtering

--- server/app.py
+++ server/app.py
@@ -1,26 +1,30 @@
 # importing dependencies
-from flask import Flask
-from flask_cors import CORS
+from flask import Flask, request, abort
+from flask_cors import CORS, cross_origin
 from sql_db import Journal
-from vector_db import pinecone_initialize, split_text, data_prep, embed_and_upsert
-import threading
+from vector_db import pinecone_initialize, split_text, data_prep, embed_and_upsert, generative_qna
 from datetime import datetime
+import threading
 import time
 
-# initialize app 
+# initialize app
 app = Flask(__name__)
 CORS(app,)
 
 last_retrieval_time = datetime.now().replace(microsecond=0)
 
-def retrieve_data_continuously(interval):
+
+def retrieve_and_upsert_continuously(interval):
     global last_retrieval_time
     while True:
         sql = Journal()
         sql.connect()
-        received_data = sql.retrieve_new_entries(last_retrieval_time=last_retrieval_time)
+        pinecone_initialize()
+        received_data = sql.retrieve_new_entries(
+            last_retrieval_time=last_retrieval_time)
 
-        if received_data: 
+        if received_data:
+
             user_ID = received_data[0]['user_id']
             content = received_data[0]['content']
             title = received_data[0]['title']
@@ -29,33 +33,29 @@ def retrieve_data_continuously(interval):
             journal_entry = title + '\n' + content
 
             metadata = {
-                        'user_id': user_ID,
-                        'content': content,
-                        'title': title,
-                        'created_at': created_at
-                    }
-            
+                'user_id': user_ID,
+                'content': content,
+                'title': title,
+                'created_at': created_at
+            }
+
             chunks = split_text(journal_entry)
+            print("Chunking Done", chunks)
 
-            prepared_data = data_prep(chunks, **metadata)
+            prepared_data = data_prep(chunks, metadata, texts=[], metadatas=[])
+            print("metadata prepared", prepared_data)
 
-            embed_and_upsert(*prepared_data)
+            embed_and_upsert(*prepared_data, user_id=user_ID)
+            print("embed and upserted", embed_and_upsert)
 
-
-
-
-
-
-
-
-        
         last_retrieval_time = datetime.now().replace(microsecond=0)
 
-
         time.sleep(interval)
-    
+
+
 def start_data_retrieval_thread(interval):
-    data_thread = threading.Thread(target=retrieve_data_continuously, args=(interval,))
+    data_thread = threading.Thread(
+        target=retrieve_and_upsert_continuously, args=(interval,))
     data_thread.daemon = True
     data_thread.start()
 
@@ -63,12 +63,21 @@ def start_data_retrieval_thread(interval):
 @app.route("/")
 def home():
 
-    pinecone_initialize()
-    print(retrieve_data_continuously)
+    return "Welcome to AI Journalling, Pinecone Initialized!"
+
+
+@cross_origin('*')
+@app.route('/ask', methods=['POST', 'GET'])
+def ask():
+    question = request.json['question'] if request.json['question'] else abort(
+        400, "Question not found!")
+    user_ID = request.json['user_id'] if request.json['user_id'] else abort(
+        400, "userID not found!")
+    response = generative_qna(question, user_ID)
+    return {"response": response}, 200
 
 
 if __name__ == '__main__':
-
+    # Run the flask app and the data retrieval thread parallely
     start_data_retrieval_thread(5)
-
     app.run(debug=True)
--- server/vector_db.py
+++ server/vector_db.py
@@ -1,14 +1,20 @@
-import pinecone
 import os
+import pinecone
 from dotenv import load_dotenv
+from uuid import uuid4
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings.openai import OpenAIEmbeddings
-from uuid import uuid4
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import RetrievalQAWithSourcesChain, RetrievalQA
+from langchain.vectorstores import Pinecone
+from langchain.embeddings.openai import OpenAIEmbeddings
 
 load_dotenv()
 
+
 class Embedding:
     pass
+
 
 def pinecone_initialize():
     index_name = 'ai-journal'
@@ -19,21 +25,24 @@ def pinecone_initialize():
     if index_name not in pinecone.list_indexes():
         pinecone.create_index(index_name, dimension=1536)
 
+    return "Pinecone Initialized!"
 
-def split_text(text):
+
+def split_text(text: str):
     text_splitter = RecursiveCharacterTextSplitter(
-        chunk_size=150,
-        chunk_overlap=10,
+        chunk_size=100,
+        chunk_overlap=2,
         separators=["\n\n", "\n", " ", ""]
     )
     chunks = text_splitter.split_text(text)
     return chunks
 
 
-def data_prep(chunks, texts: None, metadatas: None,  **metadata):
-    texts = texts or []
-    metadatas = metadatas or []
-    chunk_metadata = [{ "chunk": j, "chunk_text": text, **metadata} for j, text in enumerate(chunks)]
+def data_prep(chunks: list, metadata: dict, texts: None, metadatas: None, ):
+    texts = [] or texts
+    metadatas = [] or metadatas
+    chunk_metadata = [{"chunk": j, "chunk_text": text, **metadata}
+                      for j, text in enumerate(chunks)]
     texts.extend(chunks)
     metadatas.extend(chunk_metadata)
     ids = [str(uuid4()) for _ in range(len(texts))]
@@ -41,7 +50,7 @@ def data_prep(chunks, texts: None, metadatas: None,  **metadata):
     return ids, texts, metadatas
 
 
-def embed_and_upsert(ids, texts, metadatas):
+def embed_and_upsert(ids, texts, metadatas, user_id):
     index = pinecone.Index('ai-journal')
     model_name = 'text-embedding-ada-002'
     embed = OpenAIEmbeddings(
@@ -50,9 +59,44 @@ def embed_and_upsert(ids, texts, metadatas):
         openai_api_key=os.getenv('OPENAI_API_KEY')
     )
     embeds = embed.embed_documents(texts)
-    index.upsert(vectors=zip(ids, embeds, metadatas))
+    index.upsert(vectors=zip(ids, embeds, metadatas), namespace=user_id)
     return "Successfully embedded and upserted the journal to Pinecone!"
 
 
+def generative_qna(question, user_ID):
+    index = pinecone.Index('ai-journal')
+    model_name = 'text-embedding-ada-002'
 
+    embed = OpenAIEmbeddings(
+        document_model_name=model_name,
+        query_model_name=model_name,
+        openai_api_key=os.getenv('OPENAI_API_KEY')
+    )
 
+    text_field = "text"
+
+    vectorstore = Pinecone(
+        index, embed.embed_query, text_field, namespace=user_ID
+    )
+
+    llm = ChatOpenAI(
+        openai_api_key=os.getenv('OPENAI_API_KEY'),
+        model_name='gpt-3.5-turbo',
+        temperature=0.0
+    )
+
+    # qa_with_sources = RetrievalQAWithSourcesChain.from_chain_type(
+    #     llm=llm,
+    #     chain_type="stuff",
+    #     retriever=vectorstore.as_retriever()
+    # )
+
+    qa = RetrievalQA.from_chain_type(
+    llm=llm,
+    chain_type="stuff",
+    retriever=vectorstore.as_retriever()
+    )
+
+    response = qa.run(question)
+    print("This is the response", response)
+    return response
